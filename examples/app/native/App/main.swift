0b95ce143fd69923cab25bbc19229b96dcc9e712
Paul Cadman <git@paulcadman.dev>
2026-04-22 13:43:41 +0100

    Fix app/ example to respect system background color

--- examples/app/native/App/main.swift
+++ examples/app/native/App/main.swift
@@ -11,13 +11,14 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow(frame: UIScreen.main.bounds)
 
         let viewController = UIViewController()
-        viewController.view.backgroundColor = .white
+        viewController.view.backgroundColor = .systemBackground
 
         let label = UILabel(frame: viewController.view.bounds)
         label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         label.textAlignment = .center
         label.numberOfLines = 0
         label.font = .systemFont(ofSize: 28, weight: .semibold)
+        label.textColor = .label
         label.text = "Lean says: \(lean_ios_add_one(41))"
 
         viewController.view.addSubview(label)
